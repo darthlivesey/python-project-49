d432a4e8064bd17b2a794d05c3ead7adc3c9d136
darthlivesey <szajcev504@gmail.com>
2024-10-05 21:40:39 +0300

    3rd step

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python3
+import prompt
+
 def main():
     print("Welcome to the Brain Games!")
 
-main()
+def welcome_user():
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
