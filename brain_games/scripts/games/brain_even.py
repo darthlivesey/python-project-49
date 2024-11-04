4c2323bea65eed5b9102daabc596fdbdd909e8e1
darthlivesey <szajcev504@gmail.com>
2024-11-04 09:35:17 +0300

    Девятый шаг

--- brain_games/scripts/games/brain_even.py
+++ brain_games/scripts/games/brain_even.py
@@ -9,15 +9,16 @@ def main():
     while wins != 3:
         question_number = randint(1, 100)
         print(f'Question: {question_number}')
-        answer = input('Your answer: ')
-        condition_1 = question_number % 2 == 0 and answer == 'yes'
-        condition_2 = question_number % 2 != 0 and answer == 'no'
+        ans = input('Your answer: ')
+        condition_1 = question_number % 2 == 0 and ans == 'yes'
+        condition_2 = question_number % 2 != 0 and ans == 'no'
+        
         if condition_1 or condition_2:
             print('Correct!')
             wins += 1
         else:
             _ = 'yes' if question_number % 2 == 0 else 'no'
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{_}'")
+            print(f"'{ans}' is wrong answer ;(. Correct answer was '{_}'")
             print(f"Let's try again, {name}")
             return
 
